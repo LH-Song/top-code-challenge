759832a9ece4fc0545092faf0d5841bc08ee7934
LingHan.S <LINGKUI0324@GMAIL.COM>
2024-06-17 12:44:07 +1000

    chore: Update next.config.mjs to remove unused code

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,7 @@
-
 /** @type {import('next').NextConfig} */
 
 import withVideos from "next-videos";
 
-const nextConfig = {
-  // Your other configurations can go here
-};
+const nextConfig = {};
 
 export default withVideos(nextConfig);
