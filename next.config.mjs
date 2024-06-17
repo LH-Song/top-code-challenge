197a4eb0f1b6bbf7becf498296d53a813074f51d
LingHan.S <LINGKUI0324@GMAIL.COM>
2024-06-17 12:36:08 +1000

    chore: Update npm dependencies and add next-videos package

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+
+import withVideos from "next-videos";
+
+const nextConfig = {
+  // Your other configurations can go here
+};
+
+export default withVideos(nextConfig);
---
+++ src/app/calculator/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const CalculatorPage = () => {
+  return <div>page</div>;
+};
+
+export default CalculatorPage;
