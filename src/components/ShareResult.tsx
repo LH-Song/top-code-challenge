152f49848c061aba99df723558fe35fd2f7cfa82
LingHan.S <LINGKUI0324@GMAIL.COM>
2024-06-17 17:03:32 +1000

    chore: Update ShareResult component dependencies and optimize imports

--- src/components/ShareResult.tsx
+++ src/components/ShareResult.tsx
@@ -1,12 +1,10 @@
-import React from 'react'
+import { Button } from '@/components/ui/button'
 import html2canvas from 'html2canvas'
 import {
-  FacebookShareButton,
-  FacebookIcon,
-  TwitterShareButton,
   TwitterIcon,
+  TwitterShareButton
 } from 'next-share'
-import { Button } from '@/components/ui/button'
+import React from 'react'
 
 const ShareResult = ({
   resultRef,
@@ -30,9 +28,7 @@ const ShareResult = ({
     <div className="mt-4">
       <Button onClick={handleScreenshot}>Download Screenshot</Button>
       <div className="mt-2 flex space-x-2">
-        <FacebookShareButton url={shareUrl} hashtag="#TaxCalculator">
-          <FacebookIcon size={32} round />
-        </FacebookShareButton>
+    
         <TwitterShareButton
           url={shareUrl}
           title="Check out my tax calculation result!"
