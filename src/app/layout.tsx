2080973eb5fa56cb73623db35945936270bf988a
Dylan.S <LINGKUI0324@GMAIL.COM>
2024-06-18 21:04:48 +1000

    Add auth (#6)
    * feat: Add next-auth package for authentication
    
    Add the next-auth package to enable authentication functionality in the application. This package provides a simple and secure way to handle user authentication, including features like session management, OAuth providers, and JWT token support. Integrating next-auth will enhance the security and user experience of the application.
    
    * npm uninstall
    
    * feat: Add lazy loading for images in next.config
    
    Enable lazy loading for images in the next.config file by adding a remotePatterns property. This property specifies the hostname of the images that should be lazily loaded. This change improves the page load performance by deferring the loading of images until they are needed.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Head from 'next/head'
+import AuthProvider from './AuthProvider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,14 +17,16 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <Head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0, user-scalable=no"
-        />
-      </Head>
-      <body className={inter.className}>{children}</body>
-    </html>
+    <AuthProvider>
+      <html lang="en">
+        <Head>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0, user-scalable=no"
+          />
+        </Head>
+        <body className={inter.className}>{children}</body>
+      </html>
+    </AuthProvider>
   )
 }
