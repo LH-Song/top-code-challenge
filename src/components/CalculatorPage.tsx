db2d73e7f8f69bcff8af93848bee3f62c6c7a4bf
Dylan.S <LINGKUI0324@GMAIL.COM>
2024-06-19 13:27:26 +1000

    Refac code qaulity two (#15)
    * feat: Update lazy loading for images in gsapAnimations.ts
    
    * feat: Update lazy loading for images in gsapAnimations.ts

--- src/components/CalculatorPage.tsx
+++ src/components/CalculatorPage.tsx
@@ -24,9 +24,10 @@ import Link from 'next/link'
 import { useRef } from 'react'
 import { SignInButton, SignOutButton } from './buttons'
 import MyIncomeRecords from './MyIncomeRecords'
+import PayCycleInfo from './PayCycleInfo'
 import ResultCompare from './ResultCompare'
 import { Button } from './ui/button'
-import PayCycleInfo from './PayCycleInfo'
+import { HomeIcon } from '@/components/icon'
 
 const CalculatorPage = ({ shareUrl }: { shareUrl: string }) => {
   const resultRef = useRef<HTMLDivElement>(null)
@@ -82,20 +83,7 @@ const CalculatorPage = ({ shareUrl }: { shareUrl: string }) => {
 
         <CardFooter className="flex justify-between">
           <Link href={'/'} className="flex items-end gap-1">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="size-6 text-amber-500"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
-              />
-            </svg>
+            <HomeIcon />
             <span className="text-sm">home</span>
           </Link>
           <div className="flex items-center gap-2">
--- src/components/ShareResult.tsx
+++ src/components/ShareResult.tsx
@@ -35,9 +35,9 @@ const ShareResult = ({
         title="Check out my tax calculation result!"
         hashtags={['TaxCalculator']}
       >
-        <div className="flex w-[50vw] items-center justify-start gap-4 rounded-md bg-white px-4 py-2 text-black hover:bg-black hover:text-white lg:w-auto lg:justify-end">
+        <div className="flex w-[50vw] items-center justify-start gap-2 rounded-md bg-white px-4 py-2 text-sm text-black hover:bg-black hover:text-white lg:w-auto lg:justify-end">
           <TwitterIcon size={24} round />
-          <p>share on X</p>
+          <p>Share</p>
         </div>
       </TwitterShareButton>
     </div>
