e4b4d4951604d7a59c649c4c4974ab60a1b949bc
LingHan.S <LINGKUI0324@GMAIL.COM>
2024-06-19 14:00:39 +1000

    feat: Refactor DeductionsInput component to use formatted currency hook
    The DeductionsInput component has been refactored to use a custom hook called useFormattedCurrency. This hook takes an atom value as input and returns the formatted currency value and a setter function for the raw value. This refactoring improves code readability and reusability by abstracting the currency formatting logic into a separate hook.

--- src/components/DeductionsInput.tsx
+++ src/components/DeductionsInput.tsx
@@ -9,40 +9,42 @@ import { handleCurrencyInputChange } from '@/lib/utils/handleCurrencyInputChange
 import { useAtom } from 'jotai'
 import { useEffect, useState } from 'react'
 
+const useFormattedCurrency = (atomValue: number) => {
+  const [rawValue, setRawValue] = useState('')
+
+  useEffect(() => {
+    if (atomValue !== 0) {
+      setRawValue(formatCurrency(atomValue))
+    } else {
+      setRawValue('')
+    }
+  }, [atomValue])
+
+  return [rawValue, setRawValue] as const
+}
+
 const DeductionsInput = () => {
   const [deductions, setDeductions] = useAtom(deductionsAtom)
   const [incomeType] = useAtom(incomeTypeAtom)
   const [businessExpenses, setBusinessExpenses] = useAtom(businessExpensesAtom)
-  const [deductionsRawValue, setDeductionsRawValue] = useState('')
-  const [businessExpensesRawValue, setBusinessExpensesRawValue] = useState('')
-
-  useEffect(() => {
-    if (deductions !== 0) {
-      setDeductionsRawValue(formatCurrency(deductions))
-    } else {
-      setDeductionsRawValue('')
-    }
-  }, [deductions])
-
-  useEffect(() => {
-    if (businessExpenses !== 0) {
-      setBusinessExpensesRawValue(formatCurrency(businessExpenses))
-    } else {
-      setBusinessExpensesRawValue('')
-    }
-  }, [businessExpenses])
+  const [deductionsRawValue, setDeductionsRawValue] =
+    useFormattedCurrency(deductions)
+  const [businessExpensesRawValue, setBusinessExpensesRawValue] =
+    useFormattedCurrency(businessExpenses)
 
   return (
     <div className="flex flex-col justify-between gap-6 lg:flex-row lg:gap-12">
-      <Input
-        type="text"
-        placeholder="Deductions"
-        value={deductionsRawValue}
-        onChange={(e) =>
-          handleCurrencyInputChange(e, setDeductions, setDeductionsRawValue)
-        }
-        className="lg:w-[12rem]"
-      />
+      {incomeType !== 'contractor' && (
+        <Input
+          type="text"
+          placeholder="Deductions"
+          value={deductionsRawValue}
+          onChange={(e) =>
+            handleCurrencyInputChange(e, setDeductions, setDeductionsRawValue)
+          }
+          className="lg:w-[12rem]"
+        />
+      )}
       {incomeType === 'contractor' && (
         <Input
           type="text"
